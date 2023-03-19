1384c2bd05861089315eec3990bb4f82b506b278
Réda Maizate <reda.maizate@affluences.com>
2023-03-19 13:56:25 +0100

    feat: wip tradesbypairbymin

--- kafka-stream/src/main/scala/org/esgi/project/domain/models/Trade.scala
+++ kafka-stream/src/main/scala/org/esgi/project/domain/models/Trade.scala
@@ -2,31 +2,51 @@ package org.esgi.project.domain.models
 
 import play.api.libs.json.{Json, OFormat}
 
-import java.time.Instant
+import java.time.OffsetDateTime
 
+/* I receive this message by kafka:
+  {"partition": 0,
+   "offset": 1325351,
+   "timestamp": "2023-03-18 20:28:20",
+   "key": "BTCUSDT"
+   "value": {
+       "e": "trade",
+       "E": 1679167701103,
+       "s": "BTCUSDT",
+       "t": 3014717088,
+       "p": "27396.86000000",
+       "q": "0.00037000",
+       "b": 20340463959,
+       "a": 20340463913,
+       "T": 1679167701102,
+       "m": false,
+       "M": true
+     }
+   }
+ */
 case class Trade(
-    eventType: Instant,
-    eventTime: Instant,
+    eventType: OffsetDateTime,
+    eventTime: OffsetDateTime,
     pair: String,
-    tradeId: Integer,
+    tradeId: Int,
     price: Double,
     quantity: Double,
-    buyerOrderId: Integer,
-    sellerOrderId: Integer,
-    tradeTime: Instant,
+    buyerOrderId: Int,
+    sellerOrderId: Int,
+    tradeTime: OffsetDateTime,
     isBuyerMaker: Boolean,
     isBestMatch: Boolean
 ) {
   def apply(
-      eventType: Instant,
-      eventTime: Instant,
+      eventType: OffsetDateTime,
+      eventTime: OffsetDateTime,
       pair: String,
       tradeId: Integer,
       price: String,
       quantity: String,
       buyerOrderId: Integer,
       sellerOrderId: Integer,
-      tradeTime: Instant,
+      tradeTime: OffsetDateTime,
       isBuyerMaker: Boolean,
       isBestMatch: Boolean
   ): Trade = {
@@ -51,6 +71,14 @@ case class Trade(
   }
 }
 
+case class TradeInput(
+    e: String,
+    E: OffsetDateTime,
+    s: String
+)
+
 object Trade {
+  // TODO: Blocked here because the message value's name are not binding with Trade case class
+  //  (e != eventType, E != timestamp)
   implicit val format: OFormat[Trade] = Json.format[Trade]
 }
--- kafka-stream/src/main/scala/org/esgi/project/domain/services/ProcessStream.scala
+++ kafka-stream/src/main/scala/org/esgi/project/domain/services/ProcessStream.scala
@@ -1,30 +1,28 @@
-package org.esgi.project.domain.services
-
-import io.github.azhur.kafka.serde.PlayJsonSupport
-import jdk.jshell.spi.ExecutionControl.NotImplementedException
-import org.apache.kafka.streams.StreamsBuilder
-import org.apache.kafka.streams.scala.StreamsBuilder
-
-object ProcessStream extends PlayJsonSupport {
-  def processTradesByPairByMin(): Unit = {
-    parseTrade()
-    ...
-    throw NotImplementedException
-  }
-
-  def processMeanPriceByPairByMin(): Unit = {
-    throw NotImplementedException
-  }
-
-  def processCandleByPairByMin(): Unit = {
-    throw NotImplementedException
-  }
-
-  def processVolByPairByMin(): Unit = {
-    throw NotImplementedException
-  }
-
-  def processVolByPairByHour(): Unit = {
-    throw NotImplementedException
-  }
-}
+//package org.esgi.project.domain.services
+//
+//import io.github.azhur.kafka.serde.PlayJsonSupport
+//import jdk.jshell.spi.ExecutionControl.NotImplementedException
+//import org.apache.kafka.streams.{KafkaStreams, StreamsBuilder}
+//import org.apache.kafka.streams.scala.StreamsBuilder
+//
+//object ProcessStream extends PlayJsonSupport {
+//  def processTradesByPairByMin(stream: KafkaStreams): Unit = {
+//    throw NotImplementedException
+//  }
+//
+//  def processMeanPriceByPairByMin(): Unit = {
+//    throw NotImplementedException
+//  }
+//
+//  def processCandleByPairByMin(): Unit = {
+//    throw NotImplementedException
+//  }
+//
+//  def processVolByPairByMin(): Unit = {
+//    throw NotImplementedException
+//  }
+//
+//  def processVolByPairByHour(): Unit = {
+//    throw NotImplementedException
+//  }
+//}
